7a60c667cb43fe70e1c15b5b2695db1a6d95b8dd
CJ <christian.balagbag@98labs.com>
2014-11-12 18:30:38 +0800

    make the domain static

--- routes/default/index.js
+++ routes/default/index.js
@@ -9,16 +9,18 @@ router.get('/', function(req, res) {
 router.get('/sample', require('./sample'));
 
 router.get('/telering', function(req, res) {
- var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-  res.renderLayout('default/telering', { title: 'Telering', url : fullUrl });
+  var url = 'http://imento.ubook.no';
+  res.renderLayout('default/telering', { title: 'Telering', url : url });
 });
 
 router.get('/hbv', function(req, res) {
-  res.renderLayout('default/hbv', { title: 'HBV' });
+  var url = 'http://imento.ubook.no';
+  res.renderLayout('default/hbv', { title: 'HBV', url: url });
 });
 
 router.get('/fhi', function(req, res) {
-  res.renderLayout('default/fhi', { title: 'FHI' });
+  var url = 'http://imento.ubook.no';
+  res.renderLayout('default/fhi', { title: 'FHI', url: url });
 });
 
 module.exports = router;
