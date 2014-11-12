e40a5c673f4e5506d66afbbac73040c4867e1b1b
CJ <christian.balagbag@98labs.com>
2014-11-12 17:38:24 +0800

    add client telering

--- routes/default/index.js
+++ routes/default/index.js
@@ -8,4 +8,8 @@ router.get('/', function(req, res) {
 
 router.get('/sample', require('./sample'));
 
+router.get('/telering', function(req, res) {
+  res.renderLayout('default/telering', { title: 'Telering' });
+});
+
 module.exports = router;
